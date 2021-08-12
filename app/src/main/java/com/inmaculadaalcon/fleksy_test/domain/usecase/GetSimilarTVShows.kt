77d18932d855b09c46234ed8275834660ec82368
Inmaculada Alcon <inmaculada.alcon@module-7.com>
2021-08-12 14:29:58 +0200

    added the usecases

--- app/src/main/java/com/inmaculadaalcon/fleksy_test/domain/usecase/GetSimilarTVShows.kt
+++ app/src/main/java/com/inmaculadaalcon/fleksy_test/domain/usecase/GetSimilarTVShows.kt
@@ -3,13 +3,14 @@ package com.inmaculadaalcon.fleksy_test.domain.usecase
 import arrow.core.Either
 import com.inmaculadaalcon.fleksy_test.data.repository.MovieDBRepository
 import com.inmaculadaalcon.fleksy_test.domain.model.DomainError
-import com.inmaculadaalcon.fleksy_test.domain.model.TopRatedTVShow
+import com.inmaculadaalcon.fleksy_test.domain.model.SimilarTVShow
 import com.inmaculadaalcon.fleksy_test.domain.states.State
 import com.inmaculadaalcon.fleksy_test.domain.states.StateError
 import kotlinx.coroutines.flow.Flow
 
-class GetTopRatedTv(private val repository: MovieDBRepository) {
+class GetSimilarTVShows(private val repository: MovieDBRepository) {
 
-  fun getTopRatedTVShows(page: Int): Flow<Either<StateError<DomainError>, State<TopRatedTVShow>>> =
-    repository.getTopRatedTVShows(page)
+  fun getSimilarTVShows(idTvShow: Int, page: Int): Flow<Either<StateError<DomainError>, State<SimilarTVShow>>> =
+    repository.getSimilarTVShows(idTvShow, page)
+
 }
