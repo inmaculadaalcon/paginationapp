167d01c18c9bfa72a29ebe2ef490af2b7f490717
InmaculadaAlcon <inmaculadaalcon@gmail.com>
2021-08-13 17:26:09 +0200

    added the backgrounds

--- app/src/main/java/com/inmaculadaalcon/fleksy_test/ui/adapter/TopRatedTVShowAdapter.kt
+++ app/src/main/java/com/inmaculadaalcon/fleksy_test/ui/adapter/TopRatedTVShowAdapter.kt
@@ -1,19 +1,22 @@
 package com.inmaculadaalcon.fleksy_test.ui.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.inmaculadaalcon.fleksy_test.BuildConfig
 import com.inmaculadaalcon.fleksy_test.R
 import com.inmaculadaalcon.fleksy_test.databinding.TopRatedTvShowItemViewBinding
 import com.inmaculadaalcon.fleksy_test.domain.model.TVShow
+import java.util.*
 import kotlin.properties.Delegates
 
 class TopRatedTVShowAdapter: RecyclerView.Adapter<TopRatedTVShowAdapter.ViewHolder>() {
 
-  var items: List<TVShow> by Delegates.observable(emptyList()) {_, _, _ ->
+   var items: List<TVShow> by Delegates.observable(emptyList()) {_, _, _ ->
     notifyDataSetChanged()
   }
 
@@ -31,10 +34,14 @@ class TopRatedTVShowAdapter: RecyclerView.Adapter<TopRatedTVShowAdapter.ViewHold
 
   class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
     private val binding = TopRatedTvShowItemViewBinding.bind(view)
+    private val backgrounds = listOf(R.drawable.background, R.drawable.background_blue, R.drawable.background_green, R.drawable.background_orange, R.drawable.background_pink, R.drawable.background_purple)
 
     fun bind(tvShow: TVShow) {
+      val background = backgrounds[Random().nextInt(backgrounds.size)]
+      binding.root.background = ContextCompat.getDrawable(itemView.context, background)
       Glide.with(itemView.context).load(BuildConfig.IMAGE_BASE_URL+tvShow.posterPath).into(binding.tvshowImage)
       binding.tvshowText.text = tvShow.name
+      binding.voteAverageTextview.text = tvShow.voteAverage.toString()
     }
   }
 
