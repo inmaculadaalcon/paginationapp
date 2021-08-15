157a9ab4ad48c363f01108695a25a6c14415a72c
InmaculadaAlcon <inmaculadaalcon@gmail.com>
2021-08-15 16:14:18 +0200

    some changes of design

--- app/src/main/java/com/inmaculadaalcon/fleksy_test/ui/adapter/SimilarTVShowViewHolder.kt
+++ app/src/main/java/com/inmaculadaalcon/fleksy_test/ui/adapter/SimilarTVShowViewHolder.kt
@@ -1,48 +1,29 @@
 package com.inmaculadaalcon.fleksy_test.ui.adapter
 
-import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.inmaculadaalcon.fleksy_test.BuildConfig
 import com.inmaculadaalcon.fleksy_test.databinding.SimilarTvShowsViewHolderBinding
 import com.inmaculadaalcon.fleksy_test.domain.model.TVShow
+
 
 class SimilarTVShowViewHolder(val binding: SimilarTvShowsViewHolderBinding): RecyclerView.ViewHolder(binding.root) {
 
     private var similarTVShowItem: TVShow? = null
 
-    init {
-        binding.root.setOnClickListener {
-            val rv = binding.root.parent as RecyclerView
-            rv.smoothScrollToCenteredPosition(position)
-        }
-    }
-
-    private fun RecyclerView.smoothScrollToCenteredPosition(position: Int) {
-        val smoothScroller = object : LinearSmoothScroller(context) {
-
-            override fun calculateDxToMakeVisible(view: View?,
-                                                  snapPref: Int): Int {
-                val dxToStart = super.calculateDxToMakeVisible(view, SNAP_TO_START)
-                val dxToEnd = super.calculateDxToMakeVisible(view, SNAP_TO_END)
-
-                return (dxToStart + dxToEnd) / 2
-            }
-        }
-
-        smoothScroller.targetPosition = position
-        layoutManager?.startSmoothScroll(smoothScroller)
-    }
 
     fun bind(tvShow: TVShow) {
         this.similarTVShowItem = tvShow
         binding.root.background = ContextCompat.getDrawable(itemView.context, tvShow.backgroundColor)
-        val url = BuildConfig.IMAGE_BASE_URL+ tvShow.backdropPath
+        val urlBackground = BuildConfig.IMAGE_BASE_URL+ tvShow.backdropPath
+        val urlPoster = BuildConfig.IMAGE_BASE_URL + tvShow.posterPath
         binding.title.text = tvShow.name
+        binding.voteAverageTextview.text = tvShow.voteAverage.toString()
         binding.overviewText.text = tvShow.overview
-        Glide.with(itemView.context).load(url).into(binding.imagePoster) //TODO Add a placeholder
+        binding.originalName.text = tvShow.originalName
+        Glide.with(itemView.context).load(urlPoster).into(binding.imagePoster)
+        Glide.with(itemView.context).load(urlBackground).transform(RoundedCorners(20)).into(binding.imageBackground) //TODO Add a placeholder
     }
-
 }
